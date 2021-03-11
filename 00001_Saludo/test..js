6bdf0a1a9bfa3b48eaa4e783d39cd405d3d39617
mayra@mumuki.org <mayra@mumuki.org>
2021-03-11 13:17:47 +0000

    Mumuki Export on 2021-03-11 13:17:47 +0000

--- 00001_Saludo/test..js
+++ 00001_Saludo/test..js
@@ -1,3 +1,9 @@
-it("Descripción del ejemplo", function() {
-  assert(true)
+it("Si hacemos saludar('Alfonsina', 'Storni') debería retornar Hola Alfonsina Storni, bienvenida a Ada", function() {
+let resultado = saludar('Alfonsina', 'Storni')
+assert.equal(resultado, 'Hola Alfonsina Storni, bienvenida a Ada',saludar('Alfonsina', 'Storni') deberia retornar 'Hola Alfonsina Storni, bienvenida a Ada' pero fue ${resultado});
+})
+
+it("Si hacemos saludar('Valentina', 'Tereshkova') debería retornar Hola Valentina Tereshkova, bienvenida a Ada", function() {
+let resultado = saludar('Valentina', 'Tereshkova')
+assert.equal(resultado, 'Hola Valentina Tereshkova, bienvenida a Ada',saludar('Valentina', 'Tereshkova') deberia retornar 'Hola Valentina Tereshkova, bienvenida a Ada' pero fue ${resultado}`);
 })
