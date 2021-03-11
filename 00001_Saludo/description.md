7c9d5e561ee16af2d07b0a58330a7c7e5d8f8934
mayra@mumuki.org <mayra@mumuki.org>
2021-03-11 18:31:46 +0000

    Mumuki Export on 2021-03-11 18:31:46 +0000

--- 00001_Saludo/description.md
+++ 00001_Saludo/description.md
@@ -1,9 +1,3 @@
 > Crea un programa que pida al usuario ingresar un nombre y apellido por separado, y luego muestre un mensaje que diga, "Hola {nombre} {apellido}, bienvenida a Ada".
 
 > 
-```javascript
-ム saludar(Alfonsina,Storni)
-Hola Alfonsina Storni, bienvenida a Ada
-
-
-```
--- 00001_Saludo/test..js
+++ 00001_Saludo/test..js
@@ -1,9 +1,5 @@
-it("Si hacemos saludar('Alfonsina', 'Storni') debería retornar Hola Alfonsina Storni, bienvenida a Ada", function() {
-let resultado = saludar('Alfonsina', 'Storni')
-assert.equal(resultado, 'Hola Alfonsina Storni, bienvenida a Ada',`saludar('Alfonsina', 'Storni') deberia retornar 'Hola Alfonsina Storni, bienvenida a Ada' pero fue ${resultado}`);
-})
-
-it("Si hacemos saludar('Valentina', 'Tereshkova') debería retornar Hola Valentina Tereshkova, bienvenida a Ada", function() {
-let resultado = saludar('Valentina', 'Tereshkova')
-assert.equal(resultado, 'Hola Valentina Tereshkova, bienvenida a Ada',`saludar('Valentina', 'Tereshkova') deberia retornar 'Hola Valentina Tereshkova, bienvenida a Ada' pero fue ${resultado}`);
+describe("Descripción del grupo", function() {
+  it("Descripción del ejemplo", function() {
+    assert(true)
+  })
 })
